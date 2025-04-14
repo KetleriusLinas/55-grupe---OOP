3b56b66e6230b097268e7bb1d78a801f16b59478
Linas <ketleriuslinas@gmail.com>
2025-04-14 11:16:46 +0300

    auto-car uzdavinys

--- auto.js
+++ auto.js
@@ -1,3 +1,25 @@
 import { Car } from './js/Car.js';
 
-const volvo = new Car ();
+// volvo.engineOn();
+
+
+const volvo = new Car ('Volvo S40', 'red', 55);
+const zapas = new Car ('Zapas', 'white', 35);
+
+volvo.intro();
+console.log(volvo.intro());
+zapas.intro();
+console.log(zapas.intro());
+
+zapas.refill(5);
+zapas.refill(15);
+zapas.refill(7);
+console.log(zapas.refill(-2));
+
+console.log(zapas.refill(null));
+console.log(zapas.refill(Infinity));
+console.log(zapas.refill(NaN));
+console.log(zapas.refill('pazadais uzpilti'));
+
+
+console.log(zapas);
--- js/car.js
+++ js/car.js
@@ -1,9 +1,55 @@
-export class Car {
-
-}
-
 // exportuojam i kita faila funkcijas, constantas ir viska ka tik reikia paciam.
 // export function sum () { 
 
 // }
 // export const x = 7;
+
+/* intro() -- apibudina masinos modeli ir spalva
+
+// bako talpa,  ijungti varikli ( ar variklis ijungtas)
+    drive (km) -- nurodzius norima nuvaziuoti atstuma, yra paskaiciuojamos degalu sanaudos, jei nepakanka degalu (nera degalu, negali vaziuot)
+    
+    , tai ribiniu atveju tiesiog sustoja ir masina issiujungia.
+    engineOn() -- ijungia varikli, jei yra kuro ir jei yra isjungtas.
+    engineOff() -- isjungia varikli, jei yra ijungtas.
+    refill () -- uzpilti degalu, bet ne daugiau, nei telpa, (jei bakas tuscias.) ir jei variklis yra isjungtas.
+
+*/
+
+export class Car {
+    constructor(model, color, tankMax) {
+        this.model = model;
+        this.color = color;
+        this.tankMax = tankMax;
+        this.tank = 0;
+        this.engineIsOn = false;
+    }
+
+    intro() {
+        return `This is ${this.color} ${this.model}`
+    }
+
+    refill (volume) {
+        if (this.engineIsOn) {
+            return `Error: to refill a tank, you must turn off engine first.`;
+        }
+        
+        if (typeof volume !== 'number' || !isFinite(volume)) { // apsauga
+            return `Error only numbers allowed.`
+        }
+        if (volume < 0 ) { // apsauga nuo minusiniu skaiciu.
+            return `Refill with ${volume} litres is not allowed`;
+        }
+
+        // const availableVolume = this.tankMax - this.tank;
+        //       availableVolume
+        if (this.tankMax - this.tank >= volume) {
+            this.tank += volume;
+        } 
+        else {
+            // this.tank += availableVolume;
+            this.tank = this.tankMax;
+        }
+    }
+}
+
